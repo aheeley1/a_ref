ddc62d7213c2b902587ee7e4121dc8b54855b6e4
Adam Heeley <adamheeley@Adams-MacBook-Air.local>
2025-03-21 09:32:57 -0600

    array from set demo

--- JS/DOM1/arrays.js
+++ JS/DOM1/arrays.js
@@ -9,3 +9,11 @@
 const numbers = [1,2,3];
 const newNumbers = [...numbers, 4];  // not numbers.push(4)
 console.log(numbers);
+
+// use Set to remove a duplicate item from an array
+// instead of looping manually, simple, efficient
+const numbers2 = [1,2,2,3,4,4,5];
+const uniqueNumbers = [new Set(numbers2)];
+const uniqueNumbers2 =  Array.from(new Set(numbers));
+console.log(uniqueNumbers);
+console.log(uniqueNumbers2);
