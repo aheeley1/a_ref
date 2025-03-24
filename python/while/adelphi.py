48d9c61d93db8cd1614d6417e2b00db2e5927b60
Adam Heeley <adamheeley@Adams-MacBook-Air.local>
2025-03-24 10:03:15 -0600

    Reach use state example and another adelphi method

--- python/while/adelphi.py
+++ python/while/adelphi.py
@@ -11,5 +11,19 @@ def get_positive_nums():
             
     return results
 
-results = get_positive_nums()
-print(results)
+    #Write a program to ask for a name until the user enters END. 
+    #Print the name each time. When you are done, print "I am done."
+def print_names():
+    completed = False
+
+    while(not completed):
+        name = input("Enter a name: ")
+        if name != 'END':
+            print(name)
+        else:
+            completed = True
+            print("I am done")
+
+#results = get_positive_nums()
+#print(results)
+print_names()
